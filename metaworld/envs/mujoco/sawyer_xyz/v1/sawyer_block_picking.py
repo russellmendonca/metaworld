dc9c80d7881f26ae4da51eea51cf727ac417dbdc
Russell <rmendonc@andrew.cmu.edu>
2021-02-01 09:44:49 -0500

    running pickblock

--- metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_block_picking.py
+++ metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_block_picking.py
@@ -19,10 +19,9 @@ class SawyerBlockPickingEnv(SawyerXYZEnv):
             hand_low=hand_low,
             hand_high=hand_high,
         )
-
         self.init_config = {
             'obj_init_angle': 0.3,
-            'obj_init_pos': np.array([-0.12, 0.7, 0.02]),
+            'obj_init_pos': np.array([0, 0.7, 0.02]),
             'hand_init_pos': np.array((0, 0.6, 0.2)),
         }
         self.goal = np.array([0.12, 0.7, 0.02])
@@ -56,15 +55,13 @@ class SawyerBlockPickingEnv(SawyerXYZEnv):
     @_assert_task_is_set
     def step(self, action):
         ob = super().step(action)
-        reward, _, reachDist, pickRew, _, placingDist = self.compute_reward(action, ob)
+        reward, reachDist, placingDist = self.compute_reward(action, ob)
         self.curr_path_length += 1
 
         info = {
             'reachDist': reachDist,
-            'pickRew': pickRew,
-            'epRew': reward,
             'goalDist': placingDist,
-            'success': float(placingDist <= 0.08)
+            'success': float(placingDist <= 0.07)
         }
 
         return ob, reward, False, info
@@ -94,7 +91,7 @@ class SawyerBlockPickingEnv(SawyerXYZEnv):
     def reset_model(self):
         self._reset_hand()
         self._target_pos = self.goal.copy()
-        self.obj_init_pos = self.adjust_initObjPos(self.init_config['obj_init_pos'])
+        # self.obj_init_pos = self.adjust_initObjPos(self.init_config['obj_init_pos'])
         self.obj_init_angle = self.init_config['obj_init_angle']
         self.objHeight = self.data.get_geom_xpos('objGeom')[2]
         self.heightTarget = self.objHeight + self.liftThresh
@@ -126,87 +123,7 @@ class SawyerBlockPickingEnv(SawyerXYZEnv):
         placingGoal = self._target_pos
 
         reachDist = np.linalg.norm(objPos - fingerCOM)
+        placingDist = np.linalg.norm(objPos - placingGoal)
+        reward = -reachDist - placingDist
 
-        placingDist = np.linalg.norm(objPos[:2] - placingGoal[:-1])
-
-
-        def reachReward():
-            reachRew = -reachDist
-            reachDistxy = np.linalg.norm(objPos[:-1] - fingerCOM[:-1])
-            zRew = np.linalg.norm(fingerCOM[-1] - self.init_fingerCOM[-1])
-            if reachDistxy < 0.06:
-                reachRew = -reachDist
-            else:
-                reachRew =  -reachDistxy - zRew
-
-            # incentive to close fingers when reachDist is small
-            if reachDist < 0.05:
-                reachRew = -reachDist + max(actions[-1],0)/50
-            return reachRew , reachDist
-
-        def pickCompletionCriteria():
-            tolerance = 0.01
-            if objPos[2] >= (heightTarget- tolerance):
-                return True
-            else:
-                return False
-
-        if pickCompletionCriteria():
-            self.pickCompleted = True
-
-
-        def objDropped():
-            return (objPos[2] < (self.objHeight + 0.005)) and (placingDist >0.02) and (reachDist > 0.02)
-            # Object on the ground, far away from the goal, and from the gripper
-            # Can tweak the margin limits
-
-        def placeCompletionCriteria():
-            if abs(objPos[0] - placingGoal[0]) < 0.05 and \
-                abs(objPos[1] - placingGoal[1]) < 0.05 and \
-                objPos[2] < self.objHeight + 0.05:
-                return True
-            else:
-                return False
-
-        if placeCompletionCriteria():
-            self.placeCompleted = True
-
-        def orig_pickReward():
-            hScale = 100
-            if self.placeCompleted or (self.pickCompleted and not(objDropped())):
-                return hScale*heightTarget
-            elif (reachDist < 0.1) and (objPos[2]> (self.objHeight + 0.005)) :
-                return hScale* min(heightTarget, objPos[2])
-            else:
-                return 0
-
-        def placeReward():
-            c1 = 1000
-            c2 = 0.01
-            c3 = 0.001
-            placeRew = 1000*(self.maxPlacingDist - placingDist) + c1*(np.exp(-(placingDist**2)/c2) + np.exp(-(placingDist**2)/c3))
-            placeRew = max(placeRew,0)
-            cond = self.pickCompleted and (reachDist < 0.1) and not(objDropped())
-
-            if self.placeCompleted:
-                return [-200*actions[-1] + placeRew, placingDist]
-            elif cond:
-                if abs(objPos[0] - placingGoal[0]) < 0.05 and \
-                    abs(objPos[1] - placingGoal[1]) < 0.05:
-                    return [-200*actions[-1] + placeRew, placingDist]
-                else:
-                    return [placeRew, placingDist]
-            else:
-                return [0 , placingDist]
-
-
-        reachRew, reachDist = reachReward()
-        pickRew = orig_pickReward()
-        placeRew , placingDist = placeReward()
-
-        if self.placeCompleted:
-            reachRew = 0
-            reachDist = 0
-        reward = reachRew + pickRew + placeRew
-
-        return [reward, reachRew, reachDist, pickRew, placeRew, placingDist]
+        return [reward, reachDist, placingDist]
