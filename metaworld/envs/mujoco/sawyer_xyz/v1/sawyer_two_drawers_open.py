eb7d05ad78a62e0b1cd2d3b36f49cf33113cadc1
Russell Mendonca <russellm@berkeley.edu>
2021-02-02 05:07:16 +0300

    2 drawer task implemented

--- metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_two_drawers_open.py
+++ metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_two_drawers_open.py
@@ -8,33 +8,24 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 class SawyerTwoDrawersOpenEnv(SawyerXYZEnv):
     def __init__(self):
 
-        hand_low = (-0.5, 0.40, 0.05)
-        hand_high = (0.5, 1, 0.5)
-        obj_low = (-0.1, 0.9, 0.04)
-        obj_high = (0.1, 0.9, 0.04)
-        goal_low = (-0.1, 0.5499, 0.04)
-        goal_high = (0.1, 0.5501, 0.04)
-
         super().__init__(
             self.model_name,
-            hand_low=hand_low,
-            hand_high=hand_high,
+            hand_low=(-0.5, 0.40, 0.05),
+            hand_high=(0.5, 1, 0.5),
         )
 
+        self.drawer1_base_pos = np.array([-0.17, 0.9, 0.04])
+        self.drawer2_base_pos = np.array([ 0.17, 0.9, 0.04])
+
+        #obj_init_pos indicates the extent to which each of the drawers are open
         self.init_config = {
-            'obj_init_angle': np.array([0.3, ], dtype=np.float32),
-            'obj_init_pos': np.array([0., 0.9, 0.04], dtype=np.float32),
+            'obj_init_pos': np.array([0, 0], dtype=np.float32),
             'hand_init_pos': np.array([0, 0.6, 0.2], dtype=np.float32),
         }
-        self.obj_init_pos = self.init_config['obj_init_pos']
-        self.obj_init_angle = self.init_config['obj_init_angle']
-        self.hand_init_pos = self.init_config['hand_init_pos']
 
-        self._random_reset_space = Box(
-            np.array(obj_low),
-            np.array(obj_high),
-        )
-        self.goal_space = Box(np.array(goal_low), np.array(goal_high))
+        self.obj_init_pos  = self.init_config['obj_init_pos']
+        self.hand_init_pos = self.init_config['hand_init_pos']
+        self.goal = np.array([0.1,0.1])
 
     @property
     def model_name(self):
@@ -43,20 +34,24 @@ class SawyerTwoDrawersOpenEnv(SawyerXYZEnv):
     @_assert_task_is_set
     def step(self, action):
         ob = super().step(action)
-        reward, reachDist, pullDist = self.compute_reward(action, ob)
+        pullDist1 =  np.abs(ob[3:6] - self._target_pos1)[1]
+        pullDist2 =  np.abs(ob[6:9] - self._target_pos2)[1]
+        reward = -pullDist1 - pullDist2
+
         self.curr_path_length += 1
         info = {
-            'reachDist': reachDist,
-            'goalDist': pullDist,
-            'epRew': reward,
-            'pickRew': None,
-            'success': float(pullDist <= 0.08)
+            'pullDist1': pullDist1,
+            'pullDist2': pullDist2,
+            'goalDist' : pullDist1 + pullDist2,
+            'success': float(pullDist1 <= 0.08 and pullDist2 <= 0.08)
         }
 
         return ob, reward, False, info
 
     def _get_pos_objects(self):
-        return self.data.get_geom_xpos('handle').copy()
+        drawer1 = self.data.get_body_xpos('drawer1').copy()
+        drawer2 = self.data.get_body_xpos('drawer2').copy()
+        return np.concatenate([drawer1, drawer2])
 
     def _get_obs_dict(self):
         obs_dict = super()._get_obs_dict()
@@ -65,27 +60,13 @@ class SawyerTwoDrawersOpenEnv(SawyerXYZEnv):
 
     def reset_model(self):
         self._reset_hand()
-        self._target_pos = self.obj_init_pos - np.array([.0, .35, .0])
-        self.objHeight = self.data.get_geom_xpos('handle')[2]
+        self._target_pos = self._target_pos1 = self.drawer1_base_pos - np.array([0, self.goal[0], 0])
+        self._target_pos2 = self.drawer2_base_pos - np.array([0, self.goal[1], 0])
 
-        if self.random_init:
-            obj_pos = self._get_state_rand_vec()
-            self.obj_init_pos = obj_pos
-            goal_pos = obj_pos.copy()
-            goal_pos[1] -= 0.35
-            self._target_pos = goal_pos
+        self.sim.model.body_pos[self.model.body_name2id('drawer1')] = self.drawer1_base_pos - np.array([0,self.obj_init_pos[0], 0])
+        self.sim.model.body_pos[self.model.body_name2id('drawer2')] = self.drawer2_base_pos - np.array([0,self.obj_init_pos[1], 0])
 
-        drawer_cover_pos = self.obj_init_pos.copy()
-        drawer_cover_pos[2] -= 0.02
-        self.sim.model.body_pos[self.model.body_name2id('drawer')] = self.obj_init_pos
-        self.sim.model.body_pos[self.model.body_name2id('drawer_cover')] = drawer_cover_pos
-        self.sim.model.site_pos[self.model.site_name2id('goal')] = self._target_pos
-
-        #self.sim.model.site_pos[self.model.site_name2id('goal2')] = self._target_pos2
-
-        self.maxPullDist = 0.2
-        self.target_reward = 1000*self.maxPullDist + 1000*2
-
+        self.maxPullDist = 0.4
         return self._get_obs()
 
     def _reset_hand(self):
@@ -94,38 +75,3 @@ class SawyerTwoDrawersOpenEnv(SawyerXYZEnv):
         rightFinger, leftFinger = self._get_site_pos('rightEndEffector'), self._get_site_pos('leftEndEffector')
         self.init_fingerCOM  =  (rightFinger + leftFinger)/2
         self.reachCompleted = False
-
-    def compute_reward(self, actions, obs):
-        del actions
-
-        objPos = obs[3:6]
-        rightFinger, leftFinger = self._get_site_pos('rightEndEffector'), self._get_site_pos('leftEndEffector')
-        fingerCOM  =  (rightFinger + leftFinger)/2
-        pullGoal = self._target_pos
-        pullDist = np.abs(objPos[1] - pullGoal[1])
-        reachDist = np.linalg.norm(objPos - fingerCOM)
-        reachRew = -reachDist
-
-        self.reachCompleted = reachDist < 0.05
-
-        def pullReward():
-            c1 = 1000
-            c2 = 0.01
-            c3 = 0.001
-
-            if self.reachCompleted:
-                pullRew = 1000*(self.maxPullDist - pullDist) + c1*(np.exp(-(pullDist**2)/c2) + np.exp(-(pullDist**2)/c3))
-                pullRew = max(pullRew,0)
-                return pullRew
-            else:
-                return 0
-
-            pullRew = max(pullRew,0)
-
-            return pullRew
-
-
-        pullRew = pullReward()
-        reward = reachRew + pullRew
-
-        return [reward, reachDist, pullDist]
